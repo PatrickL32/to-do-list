aaf09e657429fec4203cfff5ef860f46cdc412be
PatrickL32 <Jedi.92.lewis@gmail.com>
2025-06-04 20:18:19 -0700

    #3

--- apps.py
+++ apps.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request , redirect, url_for
 
 server = Flask('todolist')
 
 
+todo_list=["Get Milk","walk the dog"," get a cup of coffee"] 
+
+
 @server.get("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html',todo_list_identifier=todo_list)
+
+
+@server.post("/add")
+def add_todo():
+    print("Hello from/add")
+    todo_item = request.form.get("todo")
+    if todo_item:
+        todo_list.append(todo_item)
+    return redirect(url_for("home"))
+
 
 
 @server.get("/about")
@@ -25,10 +38,5 @@ def contact():
 
             
 server.run(debug= True)
-
-
-
-
-
 #start the server
 server.run(debug=True)
